b43b5c0265a9e984cf3a795a5d0826fffe468a7e
Piotr Orzechowski <blvck2005@gmail.com>
2022-04-21 17:24:52 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+    title: `olmax`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "5IAyirAmZkF5JfQ4r1oR22DP2cxdQvfgta2l5B69rao",
+      "spaceId": ""
+    }
+  }, "gatsby-plugin-styled-components"]
+};
